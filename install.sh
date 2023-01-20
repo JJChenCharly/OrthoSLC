966460f4fd69c2edade19f219272806206a24311
JingJieChen_Charly <jingjie.chencharly@gmail.com>
2023-01-21 02:36:03 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -21,24 +21,27 @@ $1"/Step1_preparation.cpp" \
 g++ -std=c++17 -pthread \
 $1"/Step2_simple_derep.cpp" \
 -o $2"/Step2_simple_derep" && \
-g++ -std=c++17 -pthread \
-$1"/Step2_simple_derep.cpp" \
--o $2"/Step2_simple_derep" && \
 g++ -std=c++17 \
-$1"/Step3_seq_preparation.cpp" \
--o $2"/Step3_seq_preparation" && \
-cp $1"/Step4_makeblastdb.py" $2"/Step4_makeblastdb.py" && \
-cp $1"/Step4_reciprocal_blast.py" $2"/Step4_reciprocal_blast.py" && \
+$1"/Step3_seq_concat.cpp" \
+-o $2"/Step3_seq_concat" && \
 g++ -std=c++17 -pthread \
-$1"/Step5_filter_n_bin.cpp" \
--o $2"/Step5_filter_n_bin" && \
+$1"/Step4_pre_cluster.cpp" \
+-o $2"/Step4_pre_cluster" && \
+cp $1"/Step5_makeblastdb.py" $2"/Step5_makeblastdb.py" && \
+cp $1"/Step5_reciprocal_blast.py" $2"/Step5_reciprocal_blast.py" && \
 g++ -std=c++17 -pthread \
-$1"/Step6_RBF.cpp" \
--o $2"/Step6_RBF" && \
+$1"/Step6_query_binning.cpp" \
+-o $2"/Step6_query_binning" && \
 g++ -std=c++17 -pthread \
-$1"/Step7_SLC.cpp" \
--o $2"/Step7_SLC" && \
+$1"/Step7_filter_n_bin.cpp" \
+-o $2"/Step7_filter_n_bin" && \
 g++ -std=c++17 -pthread \
-$1"/Step8_write_clusters.cpp" \
--o $2"/Step8_write_clusters" && \
+$1"/Step8_RBF.cpp" \
+-o $2"/Step8_RBF" && \
+g++ -std=c++17 -pthread \
+$1"/Step9_SLC.cpp" \
+-o $2"/Step9_SLC" && \
+g++ -std=c++17 -pthread \
+$1"/Step10_write_clusters.cpp" \
+-o $2"/Step10_write_clusters" && \
 echo "Done"
